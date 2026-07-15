be15496e42d7872bd30d3f8d1f580c06747d645f
Rodrigo Marques <rodrigomas@users.noreply.github.com>
2026-07-15 12:00:05 -0300

    Add restored runtime config assets

---
+++ config/scripts/botafogo.lua
@@ -0,0 +1,29 @@
+function think(id,team)
+	--io.write("[LUA] Botafogo:" .. id .. "\n")
+	--return 1,0,0,2
+	return 1,30,-30 + id*5,2
+end
+
+function update_pos( id, x, z )
+	--io.write("[LUA] Botafogo:" .. id .. " " .. x .. " " .. z .. "\n")
+end
+
+function update_vel( id, x, z )
+	--io.write("[LUA] Botafogo:" .. id .. " " .. x .. " " .. z .. "\n")
+end
+
+function update_ball_vel( x, y, z )
+	--io.write("[LUA] Botafogo:" .. x .. " " .. y .. " " .. z .. "\n")
+end
+
+function update_ball_pos( x, y, z )
+	--io.write("[LUA] Botafogo:" .. x .. " " .. y .. " " .. z .. "\n")
+end
+
+function update_goal_pos( z )
+	--io.write("[LUA] Botafogo:" .. z .. "\n")
+end
+
+function update_enemy_pos( id, x, z )
+	--io.write("[LUA] Botafogo:" .. id .. " " .. x .. " " .. z .. "\n")
+end
---
+++ config/scripts/flamengo.lua
@@ -0,0 +1,28 @@
+function think(id,team)
+	--io.write("[LUA] Flamengo:" .. id .. "\n")
+	return 1,-30,-30 + id*5,2
+end
+
+function update_pos( id, x, z )
+	--io.write("[LUA] Flamengo:" .. id .. " " .. x .. " " .. z .. "\n")
+end
+
+function update_vel( id, x, z )
+	--io.write("[LUA] Flamengo:" .. id .. " " .. x .. " " .. z .. "\n")
+end
+
+function update_ball_vel( x, y, z )
+	--io.write("[LUA] Flamengo:" .. x .. " " .. y .. " " .. z .. "\n")
+end
+
+function update_ball_pos( x, y, z )
+	--io.write("[LUA] Flamengo:" .. x .. " " .. y .. " " .. z .. "\n")
+end
+
+function update_goal_pos( z )
+	--io.write("[LUA] Flamengo:" .. z .. "\n")
+end
+
+function update_enemy_pos( id, x, z )
+	--io.write("[LUA] Flamengo:" .. id .. " " .. x .. " " .. z .. "\n")
+end
